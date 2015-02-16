1a67e95788f0a27a90c5b7acbd7c317e67c87476
Mathias Biilmann <info@mathias-biilmann.net>
2015-02-16 12:44:55 -0800

    Small style suggestions
    Just a few small adjustments to style.
    
    Makes sure all variables are explicitly declared and that all lines are terminated with;
    
    Move the comment to the top and commented out the debug logging by default.
    
    Haven't tested this, so I could have broken stuff if I've made a typo...

--- recaptcha/recaptcha.js
+++ recaptcha/recaptcha.js
@@ -1,8 +1,8 @@
 var http = require("http");
 
 var site_key = "SITE_KEY",
-secret_key = "SECRET_KEY",
-verify_url = "https://www.google.com/recaptcha/api/siteverify";
+    secret_key = "SECRET_KEY",
+    verify_url = "https://www.google.com/recaptcha/api/siteverify";
 
 exports.captcha = function() {
   html = '<div class="g-recaptcha" data-sitekey="' + site_key + '"></div>'
@@ -12,16 +12,16 @@ exports.captcha = function() {
 }
 
 exports.validate = function(options,params,errors){
-  
-  response_string = request.params['g-recaptcha-response']
-  my_url = verify_url + "?secret=" + secret_key + "&response=" + response_string + "&remoteip=" + request.ip;
-  var response = http.get(my_url);
+// Check the response. Will only get a response if has been validated on the client side first
+// http://stackoverflow.com/questions/27297067/google-recaptcha-how-to-get-user-response-and-validate-in-the-server-side
+  var response_string = request.params['g-recaptcha-response'],
+      my_url = verify_url + "?secret=" + secret_key + "&response=" + response_string + "&remoteip=" + request.ip,
+      response = http.get(my_url);
   
   if (response["success"] == false) {
-    errors.captcha = "Error validating captcha"
+    errors.captcha = "Error validating captcha";
   }
   
-  // Check the response. Will only get a response if has been validated on the client side first
-  // http://stackoverflow.com/questions/27297067/google-recaptcha-how-to-get-user-response-and-validate-in-the-server-side
-  log( JSON.stringify(response));
+  // For debugging purposes:
+  // log( JSON.stringify(response));
 }
